c8bd1085c71c46fd6be84841d047bca2fabb76f3
piyushbishnoi515 <piyushbishnoi29129@gmail.com>
2024-06-26 15:59:02 +0530

    SLIDER DONE

---
+++ assets/js/app.js
@@ -0,0 +1,27 @@
+$('.parentslider').slick({
+    dots: false,
+    infinite: true,
+    prevArrow: ".prev",
+    nextArrow: ".next",
+    speed: 300,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ]
+});
